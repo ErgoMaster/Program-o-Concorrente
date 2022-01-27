90a8dd6bc088b431607964441619668a434ba376
ErgoMaster <gabriel.uzel1@gmail.com>
2022-01-27 00:27:39 -0300

    Iniciando projeto

---
+++ Atividade 1/src/Principal.java
@@ -0,0 +1,14 @@
+/* ***************************************************************
+* Autor............: Gabriel Uzel Fonseca
+* Matricula........: 202010028
+* Inicio...........: 27/01
+* Ultima alteracao.: XX
+* Nome.............: Calculadora de IMC
+* Funcao...........: Calcula o IMC do usuário e demonstra se ele está ou não em sobrepeso
+*************************************************************** */
+
+public class Principal {
+    public static void main(String[] args) {
+        
+    }
+}
