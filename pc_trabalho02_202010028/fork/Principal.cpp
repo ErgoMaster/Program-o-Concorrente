99617ea7b48a5e6c2a96d8cf780044bdb4e53689
ErgoMaster <gabriel.uzel1@gmail.com>
2022-03-03 20:56:39 -0300

    Começando o trabalho

---
+++ pc_trabalho02_202010028/fork/Principal.cpp
@@ -0,0 +1,19 @@
+/* ***************************************************************
+* Autor............: Gabriel Uzel Fonseca
+* Matricula........: 202010028
+* Inicio...........: 03/03/2022
+* Ultima alteracao.: xx
+* Nome.............: Processos em C
+* Funcao...........: Simulação de uma arvore genealogica utilizando processos
+*************************************************************** */
+
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    pid_t pID = fork();
+}
